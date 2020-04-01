e03780645be56f01c669e35d9a4349496f2444c3
VitaliyFedyay <fedyay.vitaliy@gmail.com>
2020-04-01 17:44:54 +0300

    paginator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { PostPageComponent } from './components/post-page/post-page.component';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { PostComponent } from './components/post/post.component';
+import { SearchPipe } from './pipes/search.pipe';
 
 
 @NgModule({
@@ -40,7 +41,8 @@ import { PostComponent } from './components/post/post.component';
     HomePageComponent,
     PostPageComponent,
     MainLayoutComponent,
-    PostComponent
+    PostComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from 'src/app/admin/model/post.model';
 import { PostService } from 'src/app/services/post.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-home-page',
@@ -11,11 +14,24 @@ import { PostService } from 'src/app/services/post.service';
 export class HomePageComponent implements OnInit {
 
   public posts$: Observable<Post>;
+  public posts;
+  public query: string;
+  public dataSource: MatTableDataSource<Post>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private postService: PostService) { }
 
   ngOnInit(): void {
     this.posts$ = this.postService.getAllPost();
+    this.posts = this.postService.getAllPost().subscribe(
+      (data) => {
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+    );
+
   }
 
 }
